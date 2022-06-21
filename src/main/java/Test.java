d90b70632bbf4706f1f721d8c7f15fb33a39ddb0
yauhenisem <yauheni.semianovich@gmail.com>
2022-06-21 13:18:19 +0300

    Added getter

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,11 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Test {
+    private String name;
+    private String surname;
+    private String sex;
+    private int age;
 }
